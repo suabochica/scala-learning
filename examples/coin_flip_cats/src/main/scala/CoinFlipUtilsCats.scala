04c4ba5a91a2170d32b3f224b06a7b0e648597bb
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-01-31 07:41:49 -0500

    coin flip: add coin flip utils cats, and put folder in the proper path

--- examples/coin_flip_cats/src/main/scala/CoinFlipUtilsCats.scala
+++ examples/coin_flip_cats/src/main/scala/CoinFlipUtilsCats.scala
@@ -1,27 +1,30 @@
-package scala.main
+package coinflip_cats
 
-import scala.util.Random
 import scala.io.StdIn.readLine
+import scala.util.Random
 
-object CoinFlipUtils {
+object CoinFlipUtilsCats {
+
   def showPrompt(): Unit = { print("\n(h)eads, (t)ails, or (q)uit: ")  }
+
   def getUserInput(): String = readLine.trim.toUpperCase
+
   def printableFlipResult(flip: String): String = flip match {
     case "H" => "Heads"
     case "T" => "Tails"
   }
 
-  // Improve this name
-  def printGameState(printableFlipResult: String, gameState: GameState): Unit = {
+  def printGameState(printableFlipResult: String, gameState: GameState): Unit =
     print(s"Flip was $printableFlipResult. ")
     printGameState(gameState)
   }
-  def printGameState(gameState: GameState): Unit = {
+
+  def printGameState(gameState: GameState): Unit =
     println(s"#Flips: ${gameState.numFlips}, #Correct: ${gameState.numCorrect}")
   }
-  def printGameOver(): Unit = println("\n=== GAME OVER ===")
 
-  // returns "H" for heads, "T" for tails
+  def printGameOver(gameState: GameState): Unit = println("\n=== GAME OVER ===")
+
   def tossCoin(r: Random): String = {
     val i = r.nextInt(2)
 
