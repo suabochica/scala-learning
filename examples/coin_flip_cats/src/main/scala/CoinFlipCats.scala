8cd16fe07b08908ce44f285a46543ec1dabd6fd3
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-02-04 19:35:09 -0500

    coin flip: add coin flip cats methods implementations

--- examples/coin_flip_cats/src/main/scala/CoinFlipCats.scala
+++ examples/coin_flip_cats/src/main/scala/CoinFlipCats.scala
@@ -18,7 +18,32 @@ object CoinFlipCats extends App {
   val r = Random
   val s = GameState(0, 0)
 
-  def mainLoop(gameState: GameState, random: Random): IO[Unit] = ???
+  def mainLoop(gameState: GameState, random: Random): IO[Unit] = for {
+    _ <- IO { showPromp() }
+    userInput <- IO { getUserInput() }
+    _ <- IO if (userInput == "H" | userInput == "T") for {
+      // First line is a hack. It shuold be a generator
+      _ <- IO { println("you said H or T") }
+      coinTossResult = tossCoin(random)
+      newNumFlips = gameState.numFlips + 1
+      newGameState = createNewGameState(
+        userInput,
+        coinTossResult,
+        gameState,
+        random,
+        newNumFlips
+      )
+      _ <- IO { printGameState(printableFlipResult(coinTossResult), newGameState) }
+      _ <- mainLoop(newGameState, random)
+    } yield Unit
+    else for {
+      _ <- IO { println("did no enter H or T")}
+      _ <- IO { printGameOver() }
+      _ <- IO { printGameState(gameState) }
+    } yield Unit
+  } yield Unit
+
+  mainLoop(s, t).unsafeRunSync()
 
   private def createNewGameState(
     userInput: String,
@@ -26,11 +51,27 @@ object CoinFlipCats extends App {
     gameState: GameState
     random: Random,
     newNumFlips: Int
-  ): GameState = ???
+  ): GameState = {
+    if (userInput == coinTossResult) {
+      val newNumCorrect = gameState.numCorrect + 1
+      val newGameState = copy(numFlips = newNumFlips, numCorrect = newNumCorrect)
+
+      newGameState
+    } else {
+      val newGameState = gameState.copy(numFlip = newNumFlips)
+
+      newGameState
+    }
+  }
 
   private def handleCoinFlip(
     gameState: GameState,
     newNumFlips: Int,
     coinTossResult: String
-    ): GameState = ???
+  ): GameState = {
+    val newNumCorrect = gameState.numCorrect + 1
+    val newGameState = copy(numFlips = newNumFlips, numCorrect = newNumCorrect)
+
+    newGameState
+  }
 }
